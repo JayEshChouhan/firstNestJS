283678381adbf6edaea0d3b0ab3bf759916778ef
Jayesh <jayesh.chouhan@cubeco.io>
2023-04-12 15:43:47 +0530

    add validation configration using class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Todo: this code for add Validattion to the application
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+  // Todo: Add configuration for Swagger
   const config = new DocumentBuilder()
     .setTitle('First Nest Project')
     .setDescription('First Nest Project Description')
